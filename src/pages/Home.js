bd33fbf904bf9a0d9de4517060f3f6367ce8b040
Felipe Merchán <felipe-sc@hotmail.es>
2020-07-23 20:00:36 -0500

    It was finished the  welcome section of the home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,20 +6,21 @@ import Recorrido from '../images/icons/t-rex-comienza-el-recorrido.svg'
 const Home = () => {
   return(
     <Fragment>
-      <section className="welcome">
+      <section className="Welcome">
         <div className="wrapper">
-          <div className="row HomeBackground__recorrido">
+          <div className="row">
             <div className="column-2">
-              <figure>
+              <figure className="Welcome__image-rexContainer">
                 <img
                   src={ Recorrido }
-                  className="HomeBackground__recorrido-rex"
+                  className="Welcome__image-rex"
                   alt="Recorrido"/>
               </figure>
             </div>
             <div className="column-4">
-              <h1>Near</h1>
-              <h2>Earth Objects</h2>
+              <h1 className="h0 mb-40">Near<br />
+                <span className="h3">Earth Objects</span>
+              </h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing 
                 elit, sed do eiusmod tempor incididunt ut labore 
@@ -27,7 +28,7 @@ const Home = () => {
               </p>
             </div>
             <div className="column-6">
-              <figure>
+              <figure className="">
                 <img
                   src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/5926fd37-e60c-4ded-9631-121245351a94/spacex-693229.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20200723%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200723T230846Z&X-Amz-Expires=86400&X-Amz-Signature=a08365382d97ea9903605eaa3a5fba537b284e6ce2f9c94daa9d0a8ad7cb2073&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22spacex-693229.png%22" 
                   alt="capsule"
