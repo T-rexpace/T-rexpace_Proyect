a627a64455fcc05d97b692b6681be44b4a3f1a59
JuanGF <jugaybre@gmail.com>
2020-07-28 16:31:17 -0500

    Updates

---
+++ precache-manifest.4a93dcdd599543a8f30051b4955ece45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b5154b18ba84f514a7d4305b1e66c6",
+    "url": "/T-rexpace_Proyect/index.html"
+  },
+  {
+    "revision": "84216054458ce3587254",
+    "url": "/T-rexpace_Proyect/static/css/main.cf7b42a4.chunk.css"
+  },
+  {
+    "revision": "9522a6ecc5cbd2b53193",
+    "url": "/T-rexpace_Proyect/static/js/2.b3939708.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/T-rexpace_Proyect/static/js/2.b3939708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84216054458ce3587254",
+    "url": "/T-rexpace_Proyect/static/js/main.d1439668.chunk.js"
+  },
+  {
+    "revision": "56ad1f8302c7e7bfbaad",
+    "url": "/T-rexpace_Proyect/static/js/runtime-main.68780dfb.js"
+  },
+  {
+    "revision": "5828f3e70242af1d670bab9551307a8d",
+    "url": "/T-rexpace_Proyect/static/media/demoAsteroide.5828f3e7.jpg"
+  },
+  {
+    "revision": "841ef2d6642f8c8e978b5b62802a964c",
+    "url": "/T-rexpace_Proyect/static/media/logotipo-negativo-color-corporativo.841ef2d6.svg"
+  },
+  {
+    "revision": "a488a13c726e29780d156e4ac19aa0ab",
+    "url": "/T-rexpace_Proyect/static/media/planeta-tierra.a488a13c.png"
+  }
+]);
