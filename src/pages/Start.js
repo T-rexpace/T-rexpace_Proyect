9f8bd4f417fe158232d86c34cccf8fe5664d7688
Felipe Merchan <felipe-sc@hotmail.es>
2021-01-02 18:22:55 -0500

    I adjusted the following styles: I removed the outline of the close button from the modal in its focus state, I adjusted the home sections to be 100vh high, I adjusted the styles of the start page to be the same as the prototype.

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -7,18 +7,18 @@ import { URL_IMAGES_REX } from '../components/atoms/UrlImages'
 
 const Start = () => {
 	return(
-		<div className="wraper">
+		<div className="wrapper">
 			<div className="row welcome">
 				<div className="column-6 text-right welcome__text">
-					<h1>BIENVENIDO</h1>
-					<h2>ACOMPAÑAME EN ESTE VIAJE</h2>
+					<h1 className="text-uppercase">Bienvenido</h1>
+					<h2 className="text-uppercase">Acompáñame en este viaje</h2>
 					<Link to="/home">
 						<Button type="normal" title="acceder" />
 					</Link>
 				</div>
-				<div className="column-6 text-center welcome__image">
-					<figure>
-						<img src={ `${URL_IMAGES_REX}t-rex-bienvenida.svg` } alt="Bienvenida" />
+				<div className="column-6 text-center">
+					<figure className="welcome__imageContainer">
+						<img className="welcome__image img-fluid" src={ `${URL_IMAGES_REX}t-rex-bienvenida.svg` } alt="Hola, te dice T-REX" width="260" />
 					</figure>
 				</div>
 			</div>
